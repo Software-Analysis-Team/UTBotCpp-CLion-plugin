0b9f882e551f038b8dcfb3a8983b4c9be5d5bb58
Volynets Arseniy <vol0ncar@yandex.ru>
2021-10-06 22:06:37 +0300

    Add action for generating tests for file

--- src/main/kotlin/com/github/vol0n/utbotcppclion/actions/SenderAction.kt
+++ src/main/kotlin/com/github/vol0n/utbotcppclion/actions/SenderAction.kt
@@ -1,10 +1,14 @@
 package com.github.vol0n.utbotcppclion.actions
 
 import com.github.vol0n.utbotcppclion.services.MyApplicationService
+import com.intellij.ide.actions.ProjectViewEditSourceAction
+import com.intellij.openapi.actionSystem.ActionPopupMenu
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.fileChooser.FileSystemTree
+import com.intellij.openapi.fileChooser.actions.FileChooserAction
 import com.intellij.openapi.vfs.VirtualFileManager
 import kotlinx.coroutines.runBlocking
 import java.io.File
@@ -39,6 +43,31 @@ class SenderAction : AnAction() {
                     VirtualFileManager.getInstance().asyncRefresh(null)
                 }
             }
+        }
+    }
+}
+
+class GenerateForFileAction: AnAction() {
+    override fun actionPerformed(p1: AnActionEvent) {
+        val file = p1.getRequiredData(CommonDataKeys.PSI_FILE)
+        val client = ApplicationManager.getApplication().getService(MyApplicationService::class.java).client
+        runBlocking {
+            client.greet(file.name)
+        }
+        println(file.fileType.name)
+    }
+
+    // action is available only if the selected file ends in .c or .cpp
+    override fun update(p0: AnActionEvent) {
+        super.update(p0)
+        val file = p0.getData(CommonDataKeys.PSI_FILE)
+        if (file != null) {
+            file.also {
+                p0.presentation.isEnabledAndVisible = it.name.endsWith(".cpp") || it.name.endsWith(".c")
+                println(it.name)
+            }
+        } else {
+            p0.presentation.isEnabled = false
         }
     }
 }
